caf4ca9c91e6a2dd2feeb9ff8d9f077fceed5deb
Claudiu Gombos <claudiu.gombos@thinslices.com>
2024-03-07 11:03:46 +0200

    note: first task with middleware

--- src/app.js
+++ src/app.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const express = require("express");
+const bodyParser = require("body-parser");
+const { sequelize } = require("./model");
+const { getProfile } = require("./middleware/getProfile");
 const app = express();
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.set("sequelize", sequelize);
+app.set("models", sequelize.models);
 
 /**
  * FIX ME!
  * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+// NOTE: this is a route and should be part of a specific "constrcats route"
+// for this i plan to create a contract repository, contract service, the contract route + unit test and integration test with supertest
+
+//  */
+// app.get('/contracts/:id',getProfile ,async (req, res) =>{
+//     const {Contract} = req.app.get('models')
+//     const {id} = req.params
+//     const contract = await Contract.findOne({where: {id}})
+//     if(!contract) return res.status(404).end()
+//     res.json(contract)
+// })
 module.exports = app;
